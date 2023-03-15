3340c9147cd5b8a9822b61e066a7211769913b2d
lortiz24 <luis.ortiz@evius.co>
2023-03-14 21:12:46 -0500

    Se crean los host para gestionar el formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [nombre, setNombre] = useState('');
+  const [semestre, setSemestre] = useState('');
+  const [deporteFavorito, setDeporteFavorito] = useState('');
+  
+
+  const deportes = [
+    'Fútbol',
+    'Baloncesto',
+    'Tenis',
+    'Voleibol',
+    'Natación'
+  ];
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // aquí se podría enviar la información a una API o hacer algo con ella
+  };
 
   return (
     <div className="App">
-      <div>
-        <form >
-          <label htmlFor="nombre" style={{margin:10}}>Nombre del estudiante</label>
-          <input name='nombre' type="text" value={'Luis'} disabled/>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="nombre" style={{margin:10}}>Nombre del estudiante</label>
+        <input name='nombre' type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} required/>
 
-          <label htmlFor="semestre" style={{margin:10}}>Semestre del estuidiante</label>
-          <input name='semestre' type="text" value={'Luis'} disabled/>
+        <label htmlFor="semestre" style={{margin:10}}>Semestre del estudiante</label>
+        <input name='semestre' type="text" value={semestre} onChange={(e) => setSemestre(e.target.value)} required/>
 
+        <label htmlFor="deporteFavorito" style={{margin:10}}>Deporte favorito</label>
+        <select name='deporteFavorito' value={deporteFavorito} onChange={(e) => setDeporteFavorito(e.target.value)} required>
+          <option value="">Seleccione un deporte</option>
+          {deportes.map((deporte, index) => (
+            <option key={index} value={deporte}>{deporte}</option>
+          ))}
+        </select>
 
-          <label htmlFor="nombre" style={{margin:10}}>Deporte favorito</label>
-          <input name='nombre' type="text" value={'Luis'} disabled/>
-
-          
-        </form>
-      </div>
+        
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
